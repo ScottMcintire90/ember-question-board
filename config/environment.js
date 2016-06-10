96946813ac0ab4e10bf0ce41d97c42f8ef7bbd66
Scott <scottmcintire90@gmail.com>
2016-06-10 08:47:10 -0700

    Initial commit

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'question-board',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://question-board-be505.firebaseio.com',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
