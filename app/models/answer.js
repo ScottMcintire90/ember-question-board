a4068037e2fbfbf9e45ab8f151ec411b4e9c4c44
Scott <scottmcintire90@gmail.com>
2016-06-10 10:28:55 -0700

    add saveQuestion method on index route

--- app/models/answer.js
+++ app/models/answer.js
@@ -4,7 +4,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   content: DS.attr(),
-  author: DS.attr(),
+  user: DS.attr(),
   date: DS.attr()
   question: DS.belongsTo('question', { async: true })
 });
