f6265848f0b9ab634766d1a62da7cac2fe95ff96
Scott <scottmcintire90@gmail.com>
2016-06-12 13:04:59 -0700

    add question and answer routes, delete methods for each

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    destroyQuestion(question) {
+      var answer_deletions = question.get('answers').map(function(answer){
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function(){
+        return question.destroyRecord();
+      });
+      this.transitionTo('index');
+    },
+
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function(){
+        return question.save();
+      });
+      this.transitionTo('question', params.question);
+    },
+
+    destroyAnswer(answer) {
+      answer.destroyRecord();
+      this.transitionTo('question');
+    },
+  }
 });
